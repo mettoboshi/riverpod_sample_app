3c3ff47dc7257c86ee0675ae50fc78e46d240c8b
Yasuo Fujita <mettoboshi.1108@gmail.com>
2022-05-13 21:01:42 +0900

    riverpodを導入してみた

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+// 値（ここでは "Hello world"）を格納する「プロバイダ」を作成します。
+// プロバイダを使うことで値のモックやオーバーライドが可能になります。
+final helloWorldProvider = Provider((_) => 'Hello world');
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends HookConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final String value2 = ref.watch(helloWorldProvider);
+
+    print(value2);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page', value: "test"),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title, required this.value})
+      : super(key: key);
 
   final String title;
+  final String value;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -41,6 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text(widget.value),
       ),
       body: Center(
         child: Column(
